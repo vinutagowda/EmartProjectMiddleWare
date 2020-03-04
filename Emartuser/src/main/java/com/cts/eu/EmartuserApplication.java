76678e394489c5e65c82df968478f8ec14175ba1
vinutagowda <60127793+vinutagowda@users.noreply.github.com>
2020-03-04 17:26:44 +0530

    Add files via upload

---
+++ Emartuser/src/main/java/com/cts/eu/EmartuserApplication.java
@@ -0,0 +1,13 @@
+package com.cts.eu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmartuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmartuserApplication.class, args);
+	}
+
+}
